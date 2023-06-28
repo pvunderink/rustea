e5654585bc8818856001da9d038ef66d3f080b1a
Pepijn Vunderink <pj.vunderink@gmail.com>
2023-06-29 01:02:38 +0200

    Refactor main

--- src/main.rs
+++ src/main.rs
@@ -16,39 +16,30 @@ use crate::{
 fn main() {
     type Genome = Vec<bool>;
 
-    // Define evaluation function
     fn evaluate(idv: &Individual<Genome, usize>) -> usize {
         idv.genotype().iter().filter(|bit| *bit).count()
     }
 
-    // Define how to compare fitness between individuals
     fn compare(idv_a: &Individual<Genome, usize>, idv_b: &Individual<Genome, usize>) -> Ordering {
-        // Higher fitness is better
-        idv_b.fitness().cmp(&idv_a.fitness())
+        idv_b.fitness().cmp(&idv_a.fitness()) // this means higher fitness is better
     }
 
-    // Setup fitness function
     let mut one_max = FitnessFunc::new(&evaluate, &compare);
 
-    // Setup variation operator
     let variation = UniformCrossover::default();
-
-    // Setup selection operator
     let selection = TruncationSelection;
 
-    // Setup genetic algorithm
     const SIZE: usize = 8192;
 
     let mut ga = SimpleGA::new(SIZE, 800, &mut one_max, selection, variation);
 
-    // Defining a target fitness allows the GA to stop early
-    ga.set_target_fitness(SIZE);
+    ga.set_target_fitness(SIZE); // defining a target fitness allows the GA to stop early
 
+    // Run EA
     let now = Instant::now();
-
     let status = ga.run(250000);
-
     let elapsed = now.elapsed();
+
     println!(
         "Best fitness: {}, Worst fitness: {}, Elapsed: {:.2?}, Status: {:?}",
         ga.best_individual().unwrap().fitness(),
